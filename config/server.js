ffb689065e4920c694bef9bcd5b05b6d126bbb7e
Amandeepsingh25 <amandeepnarang179@gmail.com>
2024-07-04 22:36:16 +0530

    updated the server.js

--- config/server.js
+++ config/server.js
@@ -1,13 +1,32 @@
-require('dotenv').config();
-
-
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+module.exports = () => ({
+  host: '0.0.0.0',
+  port: 1337,
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: ["FkCRjbndTDCiyifG+Kl1Ig==", "fe5fCRmaKad5u5na307DUQ==", "w3hBTce8o1q6MlCWmuu51g==", "zjHq0xA5sfnAOmMRLivuJA=="],
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: false,
+  },
+  apiToken: {
+    salt: 'o27BzpPVn+hV1eOOhlW6yA==',
+  },
+  admin: {
+    auth: {
+      secret: 'uBipVEdYY3YJFSQhJIg6fg==',
+    },
+  },
+  transfer: {
+    token: {
+      salt: 'OAPV5e8N5zx3nrgmJZcG+g==',
+    },
+  },
+  database: {
+    client: 'sqlite',
+    connection: {
+      filename: '.tmp/data.db',
+    },
+  },
+  jwt: {
+    secret: 'Xs38vABG+SKPR5YkO1XifA==',
   },
 });
